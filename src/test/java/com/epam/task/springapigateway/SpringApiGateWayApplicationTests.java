50d08458d6ea075360d73d087f1c88b8de15b9ed
mktol <maxim.tolstik@gmail.com>
2018-08-22 19:26:29 +0300

    first commit

---
+++ src/test/java/com/epam/task/springapigateway/SpringApiGateWayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.task.springapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringApiGateWayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
